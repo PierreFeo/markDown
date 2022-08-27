ed92367a018313ef1d64daf3ef6745ff80231e6b
Pierre <feoktistov.petr@gmail.com>
2022-08-27 18:08:06 +1000

    first commit

---
+++ app/src/main/java/com/example/testmarckdown/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.testmarckdown.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+    val test = "test"
+}
